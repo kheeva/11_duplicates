1325501d7a0b31223d3375950d3337a13c5ae1b8
kheeva <kheeva@ya.ru>
2017-11-07 17:52:25 +0300

    dups seeker and readme

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Anti-Duplicator
+Program recursively seeks duplicated files in certain folder and print result in console.
 
-[TODO. There will be project description]
+# How to Install
+You just need download and install python3. http://python.org
+
+# How to use
+Seeking duplicates in oleg's home directory:
+```
+python duplicates.py /home/oleg
+```
 
 # Project Goals
-
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,46 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+scanned_files = {}
+
+
+def has_duplicate(name, size):
+    if [name, size] in scanned_files.values():
+        return True
+    return False
+
+
+def seek_duplicates_in_dir(scanning_dir):
+    list_of_dirs = []
+    with os.scandir(scanning_dir) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file():
+                if has_duplicate(entry.name, entry.stat().st_size):
+                    print('Found the duplicate: ', entry.path)
+                else:
+                    scanned_files[entry.path] = [entry.name, entry.stat().st_size]
+            elif not entry.name.startswith('.') and entry.is_dir():
+                list_of_dirs.append(entry.path)
+    return list_of_dirs
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python duplicates.py path_to_scanning_dir.")
+    else:
+        scanning_dir = [sys.argv[1],]
+        while True:
+            new_dirs = []
+            if not scanning_dir:
+                break
+            for i in scanning_dir:
+                new_dir = seek_duplicates_in_dir(i)
+                if new_dir:
+                    new_dirs.extend(new_dir)
+            scanning_dir = new_dirs
+
 
 if __name__ == '__main__':
-    pass
+    main()
